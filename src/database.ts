689c8b4b58b93333766bd966683152f45bfb68f6
Tayo Comfort <tayoabisola44@gmail.com>
2021-12-20 11:00:56 +0100

    Migration updated

--- src/database.ts
+++ src/database.ts
@@ -13,7 +13,7 @@ const {
   ENV
 } = process.env
 
-if (ENV === 'dev') {
+if (ENV==='dev') {
   client = new Pool({
     host: POSTGRES_HOST,
     database: POSTGRES_DB,
@@ -22,7 +22,7 @@ if (ENV === 'dev') {
   })
 }
 
-if (ENV === 'test') {
+if (ENV==='test') {
   client = new Pool({
     host: POSTGRES_HOST,
     database: POSTGRES_TEST_DB,
