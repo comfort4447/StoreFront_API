eeee95ff04bf9446f28cc9b2804260d9e0fced13
Tayo Comfort <tayoabisola44@gmail.com>
2021-12-22 16:30:27 +0100

    migration updated

--- migrations/20211222144819-order-products.js
+++ migrations/20211222144819-order-products.js
@@ -19,7 +19,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20211216142932-orders-up.sql');
+  var filePath = path.join(__dirname, 'sqls', '20211222144819-order-products-up.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
@@ -34,7 +34,7 @@ exports.up = function(db) {
 };
 
 exports.down = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20211216142932-orders-down.sql');
+  var filePath = path.join(__dirname, 'sqls', '20211222144819-order-products-down.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
